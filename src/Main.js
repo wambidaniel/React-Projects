63bb220f445df6fb5302a514fbb1587ec5801da7
Daniel Wambi <wambidanielcollins256@gmail.com>
2023-01-02 11:16:40 +0300

    Calci

---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React, { component } from 'react'
+
+function Main() {
+    return (
+        <div className='row justify-content-center'>
+
+            <div className='.col-md-5'>
+
+                <h1>React Calci</h1>
+
+                <table>
+
+                    <tr>
+                        <input type="text" />
+                        <button>C</button>
+                    </tr>
+                </table>
+
+            </div>
+        </div>
+    )
+}
+export default Main
